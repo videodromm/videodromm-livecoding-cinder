21b60628b636cac26062220b1adbcc204561bccb
Bruce LANE <bruce@batchass.fr>
2016-05-16 21:50:57 +0200

    yay

--- include/VideodrommLiveCodingApp.h
+++ include/VideodrommLiveCodingApp.h
@@ -2,12 +2,19 @@
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
 
+// UserInterface
+#include "CinderImGui.h"
+// Console
 #include "VDConsole.h"
+// Settings
+#include "VDSettings.h"
 
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 using namespace VideoDromm;
+
+#define IM_ARRAYSIZE(_ARR)			((int)(sizeof(_ARR)/sizeof(*_ARR)))
 
 class VideodrommLiveCodingApp : public App {
 
@@ -19,5 +26,48 @@ public:
 	void draw() override;
 	void cleanup() override;
 private:
+	// Settings
+	VDSettingsRef				mVDSettings;
+	// handle resizing for imgui
+	void						resizeWindow();
+	bool						mIsResizing;
+	void						updateWindowTitle();
+	// imgui
+	float						color[4];
+	float						backcolor[4];
+	int							playheadPositions[12];
+	int							speeds[12];
+	// mPreviewFboWidth 80 mPreviewFboHeight 60 margin 10 inBetween 15
+	int							w;
+	int							h;
+	int							displayHeight;
+	int							xPos;
+	int							xPosCol1;
+	int							xPosCol2;
+	int							xPosCol3;
+	int							yPos;
+	int							yPosRow1;
+	int							yPosRow2;
+	int							yPosRow3;
+	int							largeW;
+	int							largeH;
+	int							largePreviewW;
+	int							largePreviewH;
+	int							margin;
+	int							inBetween;
 
+	float						f = 0.0f;
+	char						buf[64];
+
+	bool						mouseGlobal;
+	bool						removeUI;
+	// shader
+	gl::GlslProgRef				aShader;
+	//! default vertex shader
+	std::string					mPassthruVextexShaderString;
+	//! default fragment shader
+	std::string					mFboTextureFragmentShaderString;
+	string						mError;
+	// fbo
+	gl::FboRef					mFbo;
 };
--- src/VideodrommLiveCodingApp.cpp
+++ src/VideodrommLiveCodingApp.cpp
@@ -3,26 +3,339 @@
 
 void VideodrommLiveCodingApp::setup()
 {
+	// Settings
+	mVDSettings = VDSettings::create();
+	// imgui
+	margin = 3;
+	inBetween = 3;
+	// mPreviewFboWidth 80 mPreviewFboHeight 60 margin 10 inBetween 15 mPreviewWidth = 160;mPreviewHeight = 120;
+	w = mVDSettings->mPreviewFboWidth + margin;
+	h = mVDSettings->mPreviewFboHeight * 2.3;
+	largeW = (mVDSettings->mPreviewFboWidth + margin) * 4;
+	largeH = (mVDSettings->mPreviewFboHeight + margin) * 5;
+	largePreviewW = mVDSettings->mPreviewWidth + margin;
+	largePreviewH = (mVDSettings->mPreviewHeight + margin) * 2.4;
+	displayHeight = mVDSettings->mMainWindowHeight - 50;
+	yPosRow1 = 100 + margin;
+	yPosRow2 = yPosRow1 + largePreviewH + margin;
+	yPosRow3 = yPosRow2 + h*1.4 + margin;
 
+	mouseGlobal = false;
+	removeUI = false;
+	static float f = 0.0f;
+	// mouse cursor
+	if (mVDSettings->mCursorVisible) {
+		showCursor();
+	}
+	else {
+		hideCursor();
+	}
+	// load vertex shader 
+	try
+	{
+		fs::path vertexFile = getAssetPath("") / "passthru.vert";
+		if (fs::exists(vertexFile)) {
+			mPassthruVextexShaderString = loadString(loadAsset("passthru.vert"));
+			CI_LOG_V("passthru.vert loaded");
+		}
+		else
+		{
+			CI_LOG_V("passthru.vert does not exist, should quit");
+		}
+	}
+	catch (gl::GlslProgCompileExc &exc)
+	{
+		mError = string(exc.what());
+		CI_LOG_V("unable to load/compile passthru vertex shader:" + string(exc.what()));
+	}
+	catch (const std::exception &e)
+	{
+		mError = string(e.what());
+		CI_LOG_V("unable to load passthru vertex shader:" + string(e.what()));
+	}
+	// load passthru fragment shader
+	try
+	{
+		fs::path fragFile = getAssetPath("") / "live.frag";
+		if (fs::exists(fragFile)) {
+			mFboTextureFragmentShaderString = loadString(loadAsset("live.frag"));
+		}
+		else
+		{
+			mError = "live.frag does not exist, should quit";
+			CI_LOG_V(mError);
+		}
+		aShader = gl::GlslProg::create(mPassthruVextexShaderString, mFboTextureFragmentShaderString);
+		aShader->setLabel("live");
+		CI_LOG_V("live.frag loaded and compiled");
+	}
+	catch (gl::GlslProgCompileExc &exc)
+	{
+		mError = string(exc.what());
+		CI_LOG_V("unable to load/compile live fragment shader:" + string(exc.what()));
+	}
+	catch (const std::exception &e)
+	{
+		mError = string(e.what());
+		CI_LOG_V("unable to load live fragment shader:" + string(e.what()));
+	}
+	// render fbo
+	gl::Fbo::Format fboFormat;
+	mFbo = gl::Fbo::create(mVDSettings->mFboWidth, mVDSettings->mFboHeight, fboFormat.colorTexture());
+
+	mIsResizing = true;
+	getWindow()->getSignalResize().connect(std::bind(&VideodrommLiveCodingApp::resizeWindow, this));
 }
 
 void VideodrommLiveCodingApp::update()
 {
+	mVDSettings->iChannelTime[0] = getElapsedSeconds();
+	mVDSettings->iChannelTime[1] = getElapsedSeconds() - 1;
+	mVDSettings->iChannelTime[2] = getElapsedSeconds() - 2;
+	mVDSettings->iChannelTime[3] = getElapsedSeconds() - 3;
+	//
+	mVDSettings->iGlobalTime = getElapsedSeconds();
 
+	updateWindowTitle();
+}
+void VideodrommLiveCodingApp::updateWindowTitle()
+{
+	getWindow()->setTitle(to_string(getElapsedFrames()) + " " + to_string((int)getAverageFps()) + " fps Live Coding");
 }
 void VideodrommLiveCodingApp::cleanup()
 {
+	ui::Shutdown();
 	quit();
 }
 void VideodrommLiveCodingApp::mouseDown(MouseEvent event)
 {
-    
-}
 
+}
+void VideodrommLiveCodingApp::resizeWindow()
+{
+	mIsResizing = true;
+	// disconnect ui window and io events callbacks
+	ui::disconnectWindow(getWindow());
+
+	// tell the warps our window has been resized, so they properly scale up or down
+	//Warp::handleResize(mWarps);
+
+}
 void VideodrommLiveCodingApp::draw()
 {
+	if (mIsResizing) {
+		mIsResizing = false;
+		// set ui window and io events callbacks 
+		ui::connectWindow(getWindow());
+		ui::initialize();
+
+#pragma region style
+		// our theme variables
+		ImGuiStyle& style = ui::GetStyle();
+
+		style.WindowPadding = ImVec2(3, 3);
+		style.FramePadding = ImVec2(2, 2);
+		style.ItemSpacing = ImVec2(3, 3);
+		style.ItemInnerSpacing = ImVec2(3, 3);
+		style.WindowMinSize = ImVec2(w, mVDSettings->mPreviewFboHeight);
+		// new style
+		style.Alpha = 1.0;
+		style.WindowFillAlphaDefault = 0.83;
+		style.ChildWindowRounding = 3;
+		style.WindowRounding = 3;
+		style.FrameRounding = 3;
+
+		style.Colors[ImGuiCol_Text] = ImVec4(0.00f, 1.00f, 1.00f, 1.00f);
+		style.Colors[ImGuiCol_TextDisabled] = ImVec4(0.00f, 0.40f, 0.41f, 1.00f);
+		style.Colors[ImGuiCol_WindowBg] = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+		style.Colors[ImGuiCol_ChildWindowBg] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+		style.Colors[ImGuiCol_Border] = ImVec4(0.00f, 1.00f, 1.00f, 0.65f);
+		style.Colors[ImGuiCol_BorderShadow] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+		style.Colors[ImGuiCol_FrameBg] = ImVec4(0.44f, 0.80f, 0.80f, 0.18f);
+		style.Colors[ImGuiCol_FrameBgHovered] = ImVec4(0.44f, 0.80f, 0.80f, 0.27f);
+		style.Colors[ImGuiCol_FrameBgActive] = ImVec4(0.44f, 0.81f, 0.86f, 0.66f);
+		style.Colors[ImGuiCol_TitleBg] = ImVec4(0.14f, 0.18f, 0.21f, 0.73f);
+		style.Colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.00f, 0.00f, 0.00f, 0.54f);
+		style.Colors[ImGuiCol_TitleBgActive] = ImVec4(0.00f, 1.00f, 1.00f, 0.27f);
+		style.Colors[ImGuiCol_MenuBarBg] = ImVec4(0.00f, 0.00f, 0.00f, 0.20f);
+		style.Colors[ImGuiCol_ScrollbarBg] = ImVec4(0.22f, 0.29f, 0.30f, 0.71f);
+		style.Colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.00f, 1.00f, 1.00f, 0.44f);
+		style.Colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.00f, 1.00f, 1.00f, 0.74f);
+		style.Colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.00f, 1.00f, 1.00f, 1.00f);
+		style.Colors[ImGuiCol_ComboBg] = ImVec4(0.16f, 0.24f, 0.22f, 0.60f);
+		style.Colors[ImGuiCol_CheckMark] = ImVec4(0.00f, 1.00f, 1.00f, 0.68f);
+		style.Colors[ImGuiCol_SliderGrab] = ImVec4(0.00f, 1.00f, 1.00f, 0.36f);
+		style.Colors[ImGuiCol_SliderGrabActive] = ImVec4(0.00f, 1.00f, 1.00f, 0.76f);
+		style.Colors[ImGuiCol_Button] = ImVec4(0.00f, 0.65f, 0.65f, 0.46f);
+		style.Colors[ImGuiCol_ButtonHovered] = ImVec4(0.01f, 1.00f, 1.00f, 0.43f);
+		style.Colors[ImGuiCol_ButtonActive] = ImVec4(0.00f, 1.00f, 1.00f, 0.62f);
+		style.Colors[ImGuiCol_Header] = ImVec4(0.00f, 1.00f, 1.00f, 0.33f);
+		style.Colors[ImGuiCol_HeaderHovered] = ImVec4(0.00f, 1.00f, 1.00f, 0.42f);
+		style.Colors[ImGuiCol_HeaderActive] = ImVec4(0.00f, 1.00f, 1.00f, 0.54f);
+		style.Colors[ImGuiCol_Column] = ImVec4(0.00f, 0.50f, 0.50f, 0.33f);
+		style.Colors[ImGuiCol_ColumnHovered] = ImVec4(0.00f, 0.50f, 0.50f, 0.47f);
+		style.Colors[ImGuiCol_ColumnActive] = ImVec4(0.00f, 0.70f, 0.70f, 1.00f);
+		style.Colors[ImGuiCol_ResizeGrip] = ImVec4(0.00f, 1.00f, 1.00f, 0.54f);
+		style.Colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.00f, 1.00f, 1.00f, 0.74f);
+		style.Colors[ImGuiCol_ResizeGripActive] = ImVec4(0.00f, 1.00f, 1.00f, 1.00f);
+		style.Colors[ImGuiCol_CloseButton] = ImVec4(0.00f, 0.78f, 0.78f, 0.35f);
+		style.Colors[ImGuiCol_CloseButtonHovered] = ImVec4(0.00f, 0.78f, 0.78f, 0.47f);
+		style.Colors[ImGuiCol_CloseButtonActive] = ImVec4(0.00f, 0.78f, 0.78f, 1.00f);
+		style.Colors[ImGuiCol_PlotLines] = ImVec4(0.00f, 1.00f, 1.00f, 1.00f);
+		style.Colors[ImGuiCol_PlotLinesHovered] = ImVec4(0.00f, 1.00f, 1.00f, 1.00f);
+		style.Colors[ImGuiCol_PlotHistogram] = ImVec4(0.00f, 1.00f, 1.00f, 1.00f);
+		style.Colors[ImGuiCol_PlotHistogramHovered] = ImVec4(0.00f, 1.00f, 1.00f, 1.00f);
+		style.Colors[ImGuiCol_TextSelectedBg] = ImVec4(0.00f, 1.00f, 1.00f, 0.22f);
+		style.Colors[ImGuiCol_TooltipBg] = ImVec4(0.00f, 0.13f, 0.13f, 0.90f);
+		style.Colors[ImGuiCol_ModalWindowDarkening] = ImVec4(0.04f, 0.10f, 0.09f, 0.51f);
+#pragma endregion style
+	}
+	gl::clear(Color::black());
+	gl::color(Color::white());
+	gl::setMatricesWindow(mVDSettings->mMainWindowWidth, mVDSettings->mMainWindowHeight, false);
+
+#pragma region draw
+	// draw using the mix shader
+	mFbo->bindFramebuffer();
+	//gl::setViewport(mVDFbos[mVDSettings->mMixFboIndex].fbo.getBounds());
+
+	// clear the FBO
+	gl::clear();
+	gl::setMatricesWindow(mVDSettings->mFboWidth, mVDSettings->mFboHeight);
+
+	aShader->bind();
+	aShader->uniform("iGlobalTime", mVDSettings->iGlobalTime);
+	//20140703 aShader->uniform("iResolution", vec3(mVDSettings->mRenderResoXY.x, mVDSettings->mRenderResoXY.y, 1.0));
+	aShader->uniform("iResolution", vec3(mVDSettings->mFboWidth, mVDSettings->mFboHeight, 1.0));
+	aShader->uniform("iChannelResolution", mVDSettings->iChannelResolution, 4);
+	aShader->uniform("iMouse", vec4(mVDSettings->mRenderPosXY.x, mVDSettings->mRenderPosXY.y, mVDSettings->iMouse.z, mVDSettings->iMouse.z));//iMouse =  Vec3i( event.getX(), mRenderHeight - event.getY(), 1 );
+	aShader->uniform("iChannel0", 0);
+	aShader->uniform("iChannel1", 1);
+	aShader->uniform("iAudio0", 0);
+	aShader->uniform("iFreq0", mVDSettings->iFreqs[0]);
+	aShader->uniform("iFreq1", mVDSettings->iFreqs[1]);
+	aShader->uniform("iFreq2", mVDSettings->iFreqs[2]);
+	aShader->uniform("iFreq3", mVDSettings->iFreqs[3]);
+	aShader->uniform("iChannelTime", mVDSettings->iChannelTime, 4);
+	aShader->uniform("iColor", vec3(mVDSettings->controlValues[1], mVDSettings->controlValues[2], mVDSettings->controlValues[3]));// mVDSettings->iColor);
+	aShader->uniform("iBackgroundColor", vec3(mVDSettings->controlValues[5], mVDSettings->controlValues[6], mVDSettings->controlValues[7]));// mVDSettings->iBackgroundColor);
+	aShader->uniform("iSteps", (int)mVDSettings->controlValues[20]);
+	aShader->uniform("iRatio", mVDSettings->controlValues[11]);//check if needed: +1;//mVDSettings->iRatio); 
+	aShader->uniform("width", 1);
+	aShader->uniform("height", 1);
+	aShader->uniform("iRenderXY", mVDSettings->mRenderXY);
+	aShader->uniform("iZoom", mVDSettings->controlValues[22]);
+	aShader->uniform("iAlpha", mVDSettings->controlValues[4] * mVDSettings->iAlpha);
+	aShader->uniform("iBlendmode", mVDSettings->iBlendMode);
+	aShader->uniform("iChromatic", mVDSettings->controlValues[10]);
+	aShader->uniform("iRotationSpeed", mVDSettings->controlValues[19]);
+	aShader->uniform("iCrossfade", mVDSettings->controlValues[18]);
+	aShader->uniform("iPixelate", mVDSettings->controlValues[15]);
+	aShader->uniform("iExposure", mVDSettings->controlValues[14]);
+	aShader->uniform("iDeltaTime", 1.0f);// mVDAnimation->iDeltaTime);
+	aShader->uniform("iFade", (int)mVDSettings->iFade);
+	aShader->uniform("iToggle", (int)mVDSettings->controlValues[46]);
+	aShader->uniform("iLight", (int)mVDSettings->iLight);
+	aShader->uniform("iLightAuto", (int)mVDSettings->iLightAuto);
+	aShader->uniform("iGreyScale", (int)mVDSettings->iGreyScale);
+	aShader->uniform("iTransition", mVDSettings->iTransition);
+	aShader->uniform("iAnim", mVDSettings->iAnim.value());
+	aShader->uniform("iRepeat", (int)mVDSettings->iRepeat);
+	aShader->uniform("iVignette", (int)mVDSettings->controlValues[47]);
+	aShader->uniform("iInvert", (int)mVDSettings->controlValues[48]);
+	aShader->uniform("iDebug", (int)mVDSettings->iDebug);
+	aShader->uniform("iShowFps", (int)mVDSettings->iShowFps);
+	aShader->uniform("iFps", mVDSettings->iFps);
+	aShader->uniform("iTempoTime", 1.0f);// mVDAnimation->iTempoTime);
+	aShader->uniform("iGlitch", (int)mVDSettings->controlValues[45]);
+	aShader->uniform("iTrixels", mVDSettings->controlValues[16]);
+	aShader->uniform("iGridSize", mVDSettings->controlValues[17]);
+	aShader->uniform("iBeat", mVDSettings->iBeat);
+	aShader->uniform("iSeed", mVDSettings->iSeed);
+	aShader->uniform("iRedMultiplier", mVDSettings->iRedMultiplier);
+	aShader->uniform("iGreenMultiplier", mVDSettings->iGreenMultiplier);
+	aShader->uniform("iBlueMultiplier", mVDSettings->iBlueMultiplier);
+	aShader->uniform("iFlipH", 0);
+	aShader->uniform("iFlipV", 0);
+	aShader->uniform("iParam1", mVDSettings->iParam1);
+	aShader->uniform("iParam2", mVDSettings->iParam2);
+	aShader->uniform("iXorY", mVDSettings->iXorY);
+	aShader->uniform("iBadTv", mVDSettings->iBadTv);
+
+	//mRenderFbo->getColorTexture()->bind(0);
+	//mRenderFbo->getColorTexture()->bind(1);
+	gl::drawSolidRect(Rectf(0, 0, mVDSettings->mFboWidth, mVDSettings->mFboHeight));
+	// stop drawing into the FBO
+	mFbo->unbindFramebuffer();
+	//mRenderFbo->getColorTexture()->unbind();
+	//mRenderFbo->getColorTexture()->unbind();
+
+	gl::clear(Color::black());
+	gl::setMatricesWindow(toPixels(getWindowSize()));
+	gl::draw(mFbo->getColorTexture());
+
+
+#pragma endregion draw
+
+
+	// imgui
+	static int currentWindowRow1 = 0;
+	static int currentWindowRow2 = 0;
+	static int currentWindowRow3 = 0;
+	static int selectedLeftInputTexture = 2;
+	static int selectedRightInputTexture = 1;
+
+	xPos = margin;
+	ui::SetNextWindowSize(ImVec2(520, 600), ImGuiSetCond_FirstUseEver);
+	sprintf_s(buf, "Videodromm Fps %c %d###fps", "|/-\\"[(int)(ImGui::GetTime() / 0.25f) & 3], (int)getAverageFps());
+	if (!ui::Begin(buf))
+	{
+		ImGui::End();
+		return;
+	}
+	{
+		static bool read_only = false;
+		static char text[1024 * 16] = 
+		"/*\n"
+		" Fragment shader.\n"
+		"*/\n\n"
+		"void main(void) {\n"
+		//"\tvec2 uv = v_texcoord;\n"
+		"\tgl_FragColor = vec4(1.0,0.0,0.0,1.0);\n"
+		"}\n";		
+
+		ui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(0, 0));
+		ui::Checkbox("Read-only", &read_only);
+		ui::PopStyleVar();
+		if (ui::InputTextMultiline("##source", text, IM_ARRAYSIZE(text), ImVec2(-1.0f, ImGui::GetTextLineHeight() * 16), ImGuiInputTextFlags_AllowTabInput | (read_only ? ImGuiInputTextFlags_ReadOnly : 0))) {
+			// text changed
+			CI_LOG_V("text changed");
+			try
+			{
+				aShader = gl::GlslProg::create(mPassthruVextexShaderString, text);
+				aShader->setLabel("live");
+				CI_LOG_V("live.frag loaded and compiled");
+				mFboTextureFragmentShaderString = text;
+				mError = "";
+			}
+			catch (gl::GlslProgCompileExc &exc)
+			{
+				mError = string(exc.what());
+				CI_LOG_V("unable to load/compile live fragment shader:" + string(exc.what()));
+			}
+			catch (const std::exception &e)
+			{
+				mError = string(e.what());
+				CI_LOG_V("unable to load live fragment shader:" + string(e.what()));
+			}
+		}
+		else {
+			// nothing changed
+		}
+		ui::TextColored(ImColor(255, 0, 0), mError.c_str());
+	}
+	ImGui::End();
 
 }
-
 
 CINDER_APP(VideodrommLiveCodingApp, RendererGl)
