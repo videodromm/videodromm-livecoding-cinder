c548ef485a3b85286a8c1a762f3dc7c11f6d9d4d
Bruce Lane <bruce@batchass.fr>
2016-05-21 02:26:50 +0200

    xcode project update and install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+echo Install dependencies
+echo Cinder-Hap2
+git clone https://github.com/videodromm/Cinder-Hap2 ../../Cinder/blocks/Cinder-Hap2
+echo Cinder-Videodromm
+git clone https://github.com/videodromm/Cinder-Videodromm ../../Cinder/blocks/Cinder-Videodromm
+echo Cinder-Videodromm-Textures
+git clone https://github.com/videodromm/Cinder-Videodromm-Textures ../../Cinder/blocks/Cinder-Videodromm-Textures
+echo Cinder-Videodromm-Fbos
+git clone https://github.com/videodromm/Cinder-Videodromm-Fbos ../../Cinder/blocks/Cinder-Videodromm-Fbos
+echo Cinder-Videodromm-UI
+git clone https://github.com/videodromm/Cinder-Videodromm-UI ../../Cinder/blocks/Cinder-Videodromm-UI
+echo Cinder-Warping
+git clone https://github.com/paulhoux/Cinder-Warping ../../Cinder/blocks/Cinder-Warping
+echo Cinder-ImGui
+git clone https://github.com/simongeilfus/Cinder-ImGui ../../Cinder/blocks/Cinder-ImGui
+echo Cinder-MIDI2
+git clone https://github.com/brucelane/Cinder-MIDI2 ../../Cinder/blocks/Cinder-MIDI2
+echo Cinder-WebSocketPP
+git clone https://github.com/videodromm/Cinder-WebSocketPP ../../Cinder/blocks/Cinder-WebSocketPP
+echo Required Cinder blocks checked out
