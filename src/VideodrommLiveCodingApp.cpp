9cdf6ea5e5c2cc71e85cfbf5caddf6c3af6af29e
Bruce LANE <bruce@batchass.fr>
2016-10-11 12:32:32 +0200

    editor in VD block

--- src/VideodrommLiveCodingApp.cpp
+++ src/VideodrommLiveCodingApp.cpp
@@ -330,72 +330,7 @@ void VideodrommLiveCodingApp::drawMain()
 	mVDUI->Run("UI", (int)getAverageFps());
 	if (mVDUI->isReady()) {
 
-#pragma region Editor
-		/*ui::SetNextWindowPos(ImVec2(mVDSettings->uiXPosCol1, mVDSettings->uiYPosRow2), ImGuiSetCond_Once);
-		ui::SetNextWindowSize(ImVec2(mVDSettings->uiLargeW * 3, mVDSettings->uiLargeH), ImGuiSetCond_FirstUseEver);
-		ui::Begin("Editor");
-		{
-			static bool read_only = false;
 
-			size_t const MAX = 32768; // maximum number of chars
-			static char mShaderText[MAX] =
-				"uniform vec3 iResolution;\n"
-				"uniform vec3 iColor;\n"
-				"uniform float iGlobalTime;\n"
-				"uniform sampler2D iChannel0;\n"
-				"uniform sampler2D iChannel1;\n"
-				"uniform sampler2D iChannel2;\n"
-				"uniform vec3 spectrum;\n"
-				"\n"
-				"out vec4 oColor;\n"
-				"void main(void) {\n"
-				"\tvec2 uv = gl_FragCoord.xy / iResolution.xy;\n"
-				"\tvec4 t0 = texture(iChannel0, uv);\n"
-				"\tvec4 t1 = texture(iChannel1, uv);\n"
-				"\tvec4 t2 = texture(iChannel2, uv);\n"
-				"\toColor = vec4(t0.x, t1.y, cos(iGlobalTime), 1.0);\n"
-				"}\n";
-			if (mShaderTextToLoad) {
-				mShaderTextToLoad = false;
-				std::copy(mFboTextureFragmentShaderString.begin(), (mFboTextureFragmentShaderString.size() >= MAX ? mFboTextureFragmentShaderString.begin() + MAX : mFboTextureFragmentShaderString.end()), mShaderText);
-			}
-			ui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(0, 0));
-			ui::Checkbox("Read-only", &read_only);
-			ui::PopStyleVar();
-			ui::TextColored(ImVec4(1.0f, 0.0f, 1.0f, 1.0f), "uniform");
-			if (ui::InputTextMultiline("##source", mShaderText, IM_ARRAYSIZE(mShaderText), ImVec2(-1.0f, -1.0f), ImGuiInputTextFlags_AllowTabInput | (read_only ? ImGuiInputTextFlags_ReadOnly : 0))) {
-				// text changed // TODO height ? mVDSettings->uiYPosRow2 - 200.0f
-				CI_LOG_V("text changed");
-				try
-				{
-					aShader = gl::GlslProg::create(mPassthruVextexShaderString, mShaderText);
-					aShader->setLabel("live");
-					CI_LOG_V("live.frag loaded and compiled");
-					mFboTextureFragmentShaderString = mShaderText;
-					stringstream sParams;
-					sParams << "/*{ \"title\" : \"" << getElapsedSeconds() << "\" }*//* " << mFboTextureFragmentShaderString;
-					mVDRouter->wsWrite(sParams.str());
-					//OK mVDRouter->wsWrite("/*{ \"title\" : \"live\" }*//* " + mFboTextureFragmentShaderString);
-					mError = "";
-				}
-				catch (gl::GlslProgCompileExc &exc)
-				{
-					mError = string(exc.what());
-					CI_LOG_V("unable to load/compile live fragment shader:" + string(exc.what()));
-				}
-				catch (const std::exception &e)
-				{
-					mError = string(e.what());
-					CI_LOG_V("unable to load live fragment shader:" + string(e.what()));
-				}
-			}
-			else {
-				// nothing changed 
-			}
-			ui::TextColored(ImColor(255, 0, 0), mError.c_str());
-		}
-		ui::End();*/
-#pragma endregion Editor
 	}
 
 }
